f5d164987f4e202dbe69bd83ce984980682017a4
NickWeber651 <N.Weber@Student.HTW-Berlin.de>
2026-03-10 17:16:19 +0100

    feat(util): currentMinute() zu TimeProvider hinzugefuegt
    Ermoeglicht minutengenaue Berechnungen fuer den Zeitfenster-Start. FakeTimeProvider um minute-Parameter erweitert.

--- app/src/main/java/de/nick/waterreminderapp/util/TimeProvider.kt
+++ app/src/main/java/de/nick/waterreminderapp/util/TimeProvider.kt
@@ -4,6 +4,7 @@ import java.util.Calendar
 
 interface TimeProvider {
     fun currentHour(): Int
+    fun currentMinute(): Int
     fun currentDayOfWeek(): Int
     fun currentDayOfYear(): Int
     fun currentYear(): Int
@@ -11,6 +12,7 @@ interface TimeProvider {
 
 object SystemTimeProvider : TimeProvider {
     override fun currentHour()      = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+    override fun currentMinute()    = Calendar.getInstance().get(Calendar.MINUTE)
     override fun currentDayOfWeek() = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
     override fun currentDayOfYear() = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)
     override fun currentYear()      = Calendar.getInstance().get(Calendar.YEAR)
--- app/src/test/java/de/nick/waterreminderapp/util/FakeTimeProvider.kt
+++ app/src/test/java/de/nick/waterreminderapp/util/FakeTimeProvider.kt
@@ -6,14 +6,17 @@ class FakeTimeProvider(
     private var hour: Int,
     private var dayOfWeek: Int,
     private var dayOfYear: Int,
-    private var year: Int
+    private var year: Int,
+    private var minute: Int = 0
 ) : TimeProvider {
     override fun currentHour()      = hour
+    override fun currentMinute()    = minute
     override fun currentDayOfWeek() = dayOfWeek
     override fun currentDayOfYear() = dayOfYear
     override fun currentYear()      = year
 
     fun setHour(h: Int)      { hour = h }
+    fun setMinute(m: Int)    { minute = m }
     fun setDayOfWeek(d: Int) { dayOfWeek = d }
     fun advanceToDayOfYear(d: Int, y: Int = year) { dayOfYear = d; year = y }
 }
